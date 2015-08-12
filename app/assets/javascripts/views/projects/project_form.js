6b585ec78ab98a6d75a0d5baf2c30e2fc17e910d
ralam <rehan.alam@gmail.com>
2015-08-12 09:52:25 -0700

    Check that form data is collected properly

--- app/assets/javascripts/views/projects/project_form.js
+++ app/assets/javascripts/views/projects/project_form.js
@@ -5,7 +5,7 @@ Enginestarter.Views.ProjectForm = Backbone.View.extend({
   className: 'new-project form-inline',
 
   events: {
-    'click submit': 'submitForm'
+    'submit': 'submitForm'
   },
 
   render: function () {
@@ -19,5 +19,19 @@ Enginestarter.Views.ProjectForm = Backbone.View.extend({
 
   submitForm: function (event) {
     event.preventDefault();
+
+    var formData = $(event.currentTarget).serializeJSON();
+    var projectData = formData.project;
+    projectData.category_id = 1; //placeholder;
+    console.log(projectData);
+    // var model = new Enginestarter.Models.Project();
+    // model.save(projectData, {
+    //   success: function (project) {
+    //     this.collection.add(project) {
+    //       Backbone.history.navigate('/', { trigger: true})
+    //     }
+    //   }
+    // });
+
   }
 });
