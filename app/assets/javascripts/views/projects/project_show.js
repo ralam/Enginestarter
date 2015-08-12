be0779d732e8fefd56dd72c9476ef641004128d5
ralam <rehan.alam@gmail.com>
2015-08-12 11:58:54 -0700

    return date formated with timezone in API controller

--- app/assets/javascripts/views/projects/project_show.js
+++ app/assets/javascripts/views/projects/project_show.js
@@ -1,13 +1,30 @@
 Enginestarter.Views.ProjectShow = Backbone.View.extend({
   template: JST['projects/show'],
 
+  className: '.container-fluid',
+
   initialize: function () {
     this.listenTo(this.model, 'sync', this.render)
   },
 
   render: function () {
-    this.$el.html(this.template({ project: this.model }));
+    var dbDate = this.model.attributes.end_date
+    var endDate = new Date(dbDate);
+    var futureDate = new Date();
+    var daysLeft = futureDate.setTime(endDate - (new Date()));
+    console.log(dbDate);
+    console.log(endDate);
+    this.$el.html(this.template({ project: this.model, days: daysLeft }));
 
     return this;
+  },
+
+  parseDate: function (str) {
+    var mdy = str.split('/')
+    return new Date(mdy[2], mdy[0]-1, mdy[1]);
+  },
+
+ daydiff: function (first, second) {
+    return (second-first)/(1000*60*60*24);
   }
 })
--- app/views/api/projects/show.json.jbuilder
+++ app/views/api/projects/show.json.jbuilder
@@ -1,1 +1,3 @@
 json.partial!('api/projects/projects', project: @project)
+
+json.formatted_date("#{@project.end_date.to_s} #{Time.now.strftime("%z")}")
