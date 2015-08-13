463a0e47353badfdee7044be2c57db75c45cc313
ralam <rehan.alam@gmail.com>
2015-08-13 11:41:34 -0700

    change project show view to composite view, render reward subviews

--- app/assets/javascripts/models/project.js
+++ app/assets/javascripts/models/project.js
@@ -1,3 +1,21 @@
 Enginestarter.Models.Project = Backbone.Model.extend({
-  urlRoot: 'api/projects'
+  urlRoot: 'api/projects',
+
+  rewards: function () {
+    if (!this._rewards) {
+      this._rewards = new Enginestarter.Collections.Rewards([], { project: this});
+    }
+
+    return this._rewards
+  },
+
+  parse: function (response) {
+    if (response.rewards) {
+      this.rewards().set(response.rewards, { parse: true });
+      delete response.rewards;
+    }
+
+    return response;
+  }
+
 });
--- app/assets/javascripts/views/projects/project_show.js
+++ app/assets/javascripts/views/projects/project_show.js
@@ -7,19 +7,23 @@ Enginestarter.Views.ProjectShow = Backbone.CompositeView.extend({
     this.listenTo(this.model, 'sync', this.render)
   },
 
-  addReward: function () {
+  addReward: function (reward) {
     var view = new Enginestarter.Views.RewardShow({
-      model: reward;
+      model: reward
     });
-    this.addSubView('#rewards', view)
+    this.addSubview('#rewards', view);
   },
 
   render: function () {
     var date = this.model.attributes.formatted_date
     var endDate = new Date(date);
     var daysLeft = Math.floor((endDate - Date.now()) / 86400000) + 1;
+    var that = this;
+
+
     this.$el.html(this.template({ project: this.model, days: daysLeft }));
 
+    this.model.rewards().each(this.addReward.bind(this));
     return this;
   }
 })
