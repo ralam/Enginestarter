b3a25527b04569ddd62649982eeb377699837ebe
ralam <rehan.alam@gmail.com>
2015-08-14 14:30:38 -0700

    Fix seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-Cateogry.create(title: 'Adventure')
+Category.create(title: 'Adventure')
