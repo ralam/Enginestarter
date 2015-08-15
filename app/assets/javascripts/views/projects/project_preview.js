01937783a9cd9c55f04037db0cbb894cb15fae31
ralam <rehan.alam@gmail.com>
2015-08-15 11:03:28 -0700

    style project  index page

--- app/assets/javascripts/views/projects/project_preview.js
+++ app/assets/javascripts/views/projects/project_preview.js
@@ -1,7 +1,7 @@
 Enginestarter.Views.ProjectPreview = Backbone.View.extend({
   template: JST['projects/preview'],
 
-  tagName: 'project-preview',
+  className: 'container project-preview col-sm-3 title',
 
   render: function () {
     this.$el.html(this.template({ project: this.model }));
