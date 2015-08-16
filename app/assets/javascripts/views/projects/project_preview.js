47ebb4510ae57f54dcacfec490f90222bf1385c8
ralam <rehan.alam@gmail.com>
2015-08-16 09:29:57 -0700

    style previews onn index page

--- app/assets/javascripts/views/projects/project_preview.js
+++ app/assets/javascripts/views/projects/project_preview.js
@@ -1,7 +1,7 @@
 Enginestarter.Views.ProjectPreview = Backbone.View.extend({
   template: JST['projects/preview'],
 
-  className: 'project-preview col-sm-3 title',
+  className: 'project-preview col-sm-2 title',
 
   render: function () {
     this.$el.html(this.template({ project: this.model }));
