5a5a1f130929b3cb7b7e021fdb0b10ec44c7b012
ralam <rehan.alam@gmail.com>
2015-08-17 09:41:18 -0700

    modify page to allow multiple rewardings

--- app/assets/javascripts/views/rewards/reward_show.js
+++ app/assets/javascripts/views/rewards/reward_show.js
@@ -14,6 +14,17 @@ Enginestarter.Views.RewardShow = Backbone.View.extend({
   },
 
   supportProject: function (event) {
-    this.model.rewardings().save({reward_id: this.model.id });
-  },
+    this.model.rewardings().save({reward_id: this.model.id }, {
+      success: function () {
+        var projectId = this.model.attributes.project_id
+        this.model._rewardings = undefined;
+        Backbone.history.navigate('someDeadRoute');
+        Backbone.history.navigate('projects/' + projectId , {trigger: true});
+        this.render();
+      }.bind(this),
+      error: function (errors, errorText) {
+        console.log(errorText)
+      }
+    });
+  }
 });
