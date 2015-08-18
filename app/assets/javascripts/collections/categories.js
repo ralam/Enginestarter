9526c32e87a7f437c5ed4846bb379a7817faa7b7
ralam <rehan.alam@gmail.com>
2015-08-18 09:42:39 -0700

    add #getOrFetch to category collection

--- app/assets/javascripts/collections/categories.js
+++ app/assets/javascripts/collections/categories.js
@@ -2,7 +2,22 @@ Enginestarter.Collections.Categories = Backbone.Collection.extend({
   url: 'api/categories',
 
   getOrFetch: function (id) {
-    
+    var collection = this;
+    var category = this.get(id);
+
+    if (category) {
+      category.fetch();
+    } else {
+      category = new Enginestarter.Models.Project({ id: id });
+      collection.add(category);
+      category.fetch({
+        error: function () {
+          collection.remove(category);
+        }
+      });
+    }
+
+    return category;
   }
 });
 
