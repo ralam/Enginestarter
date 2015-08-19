999f0073ba97a7994a78b6778253149dcf10830a
ralam <rehan.alam@gmail.com>
2015-08-19 14:49:38 -0700

    add user show Backbone view

---
+++ app/assets/javascripts/views/users/user_show.js
@@ -0,0 +1,9 @@
+Enginestarter.Views.UserShow = Backbone.View.extend({
+  template: JST['users/show'],
+
+  render: function () {
+    this.$el.html(this.template({ user: this.model }));
+
+    return this;
+  }
+})
