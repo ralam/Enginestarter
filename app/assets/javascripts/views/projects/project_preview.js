85d36b06ddc0b9bb55944dd741766871da379135
ralam <rehan.alam@gmail.com>
2015-08-19 10:19:07 -0700

    adjust date in preview to use real data

--- app/assets/javascripts/views/projects/project_preview.js
+++ app/assets/javascripts/views/projects/project_preview.js
@@ -3,7 +3,19 @@ Enginestarter.Views.ProjectPreview = Backbone.View.extend({
   className: 'project-preview title',
 
   render: function () {
-    this.$el.html(this.template({ project: this.model }));
+    var date = this.model.attributes.formatted_date
+    var endDate = new Date(date);
+    this.daysLeft = Math.floor((endDate - Date.now()) / 86400000) + 1;
+
+    if (this.daysLeft < 0) {
+      this.remove();
+      return;
+    }
+
+    this.$el.html(this.template({
+      project: this.model,
+      days: this.daysLeft
+    }));
 
     return this;
   }
