1f0e5cf97cf12e24b4735b56143c3e1a14bd6456
ralam <rehan.alam@gmail.com>
2015-08-19 14:58:36 -0700

    rerender user show page on user sync

--- app/assets/javascripts/views/users/user_show.js
+++ app/assets/javascripts/views/users/user_show.js
@@ -1,6 +1,10 @@
 Enginestarter.Views.UserShow = Backbone.View.extend({
   template: JST['users/show'],
 
+  initialize: function () {
+    this.listenTo(this.model, 'sync', this.render)
+  },
+
   render: function () {
     this.$el.html(this.template({ user: this.model }));
 
