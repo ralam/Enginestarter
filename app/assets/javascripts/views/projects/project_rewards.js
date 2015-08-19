05b23e19510f30f5fd86c94ae5f117d62fcc909b
ralam <rehan.alam@gmail.com>
2015-08-19 11:35:45 -0700

    populate rewards on project reward list

--- app/assets/javascripts/views/projects/project_rewards.js
+++ app/assets/javascripts/views/projects/project_rewards.js
@@ -8,7 +8,23 @@ Enginestarter.Views.ProjectRewardList = Backbone.CompositeView.extend({
 
   render: function () {
     this.$el.html(this.template({ project: this.model }));
-
+    this.renderRewards();
     return this;
-  }
+  },
+
+  addReward: function (reward) {
+    var view = new Enginestarter.Views.RewardShow({
+      model: reward,
+      daysLeft: this.daysLeft,
+      active: true
+    });
+    this.addSubview('.rewards-list', view);
+  },
+
+  renderRewards: function () {
+    this.model.rewards().models.sort(
+      function(a, b) {return a.attributes.level - b.attributes.level}
+    );
+    this.model.rewards().each(this.addReward.bind(this));
+  },
 })
