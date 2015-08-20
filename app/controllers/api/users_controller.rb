51d7d12fd8e036b20bd30354582e8d79520d91b3
ralam <rehan.alam@gmail.com>
2015-08-19 17:00:59 -0700

    add Rails API controller validation for current user

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,10 +1,20 @@
 class Api::UsersController < ApplicationController
+  before_action :require_login_as_user
+
   def show
     @user = User.find(params[:id])
     if @user
       render :show
     else
       render json: ["That user doesn't exist"]
+    end
+  end
+
+  private
+
+  def require_login_as_user
+    if params[:id].to_i != current_user.id
+      render json: ["You may not view other user's profiles"]
     end
   end
 end
